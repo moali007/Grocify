12298b6a1a24658d53215c04088335875018f670
Mohammad Ali <asimali104013@gmail.com>
2025-05-27 20:09:16 +0530

    updated frontend url

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectCloudinary();
 // Allow multiple origins
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://grocify-wine.vercel.app/",
+  "https://grocify-wine.vercel.app",
 ];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
